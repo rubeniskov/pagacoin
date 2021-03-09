e7233c765692b63d4be3d6e302e9d73e7150a3ac
rubeniskov <me@rubeniskov.com>
2021-03-09 18:11:00 +0100

    chore(app): add configuration

---
+++ app/babel.config.js
@@ -0,0 +1,11 @@
+
+
+module.exports = function(api) {
+  api.cache(true);
+
+  return {
+    presets: ['@babel/react', '@babel/typescript'],
+  };
+};
+
+
---
+++ app/webpack.config.js
@@ -0,0 +1,71 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack')
+
+module.exports = () => ({
+  mode: process.env.NODE_ENV ? process.env.NODE_ENV : 'development',
+  entry: "./src/App.tsx",
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    hot: true,
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+    fallback: {
+      "process": require.resolve("process/browser")
+    }
+  },
+  module: {
+    rules: [{
+      test: /(\.tsx?|node_modules\/react-router-native\/.*js)$/,
+      use: {
+        loader: 'babel-loader'
+      }
+    },  {
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+        },
+      ],
+    },]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: ['process'],
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    port: 9966,
+    historyApiFallback: true,
+    proxy: {
+      "/api": {
+        changeOrigin: true,
+        cookieDomainRewrite: "localhost",
+        target: "http://localhost:8080/",
+        pathRewrite: { '^/api': '' },
+        onProxyReq: proxyReq => {
+          // Browers may send Origin headers even with same-origin
+          // requests. To prevent CORS issues, we have to change
+          // the Origin to match the target URL.
+          if (proxyReq.getHeader('origin')) {
+            proxyReq.setHeader('origin', gdc);
+          }
+        }
+      },
+    }
+  }
+})
